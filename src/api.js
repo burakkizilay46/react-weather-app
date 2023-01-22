0a937cb8f0fef37ef4a94b83aa56bd783ca6ca95
Burak Kızılay <burak.kizilay@westerops.com>
2023-01-23 00:15:52 +0300

    search bar added

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "da552737bbmsh81db33992c2df14p1d8329jsn3104f22f26a5",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
